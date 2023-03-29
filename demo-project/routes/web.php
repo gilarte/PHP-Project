280b1c229c7d1adb2c73066924e1b48704c9aded
gilarte <rafagilarte@gmail.com>
2023-03-29 13:49:24 +0200

    doing register

--- demo-project/routes/web.php
+++ demo-project/routes/web.php
@@ -17,4 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/register', function () {
+    return view('register');
+});
+
+
 Route::view('/resources/views', 'register.blade.php');
